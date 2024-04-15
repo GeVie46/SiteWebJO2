19b9fba03a2953846d83c90ceddd16ece0c01367
GeVie46 <gg182@hotmail.fr>
2024-04-15 16:27:36 +0200

    add userKey creation, update Identity register forms

--- SiteWebJO2/Models/ApplicationUser.cs
+++ SiteWebJO2/Models/ApplicationUser.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
 
 namespace SiteWebJO2.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
         public string Name { get; set; }
+        [PersonalData]
         public string Lastname { get; set; }
         public Byte[] Userkey { get; set; }
+
+        //https://learn.microsoft.com/fr-fr/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-8.0
+        public static byte[] GenerateUserKey()
+        {
+            // Generate a 128-bit salt using a sequence of
+            // cryptographically strong random bytes.
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+            return salt;
+        }
     }
+
+    
 }
